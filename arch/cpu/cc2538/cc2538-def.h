19c6f75e09433fd64888aa68fd4dd87e9b3835fc
George Oikonomou <george@contiki-ng.org>
2018-10-13 13:35:24 +0100

    Update CC2538 to the new GPIO HAL port/pin numbering

--- arch/cpu/cc2538/cc2538-def.h
+++ arch/cpu/cc2538/cc2538-def.h
@@ -68,8 +68,10 @@
 /* Path to headers with implementation of mutexes and memory barriers */
 #define MUTEX_CONF_ARCH_HEADER_PATH          "mutex-cortex.h"
 #define MEMORY_BARRIER_CONF_ARCH_HEADER_PATH "memory-barrier-cortex.h"
-
+/*---------------------------------------------------------------------------*/
 #define GPIO_HAL_CONF_ARCH_HDR_PATH          "dev/gpio-hal-arch.h"
+#define GPIO_HAL_CONF_ARCH_SW_TOGGLE         1
+#define GPIO_HAL_CONF_PORT_PIN_NUMBERING     0
 /*---------------------------------------------------------------------------*/
 #endif /* CC2538_DEF_H_ */
 /*---------------------------------------------------------------------------*/
