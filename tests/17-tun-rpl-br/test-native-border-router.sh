7a9ae6622f5ce86bef1916d561230ae4e9a9f437
Joakim Eriksson <ruben.joakim.eriksson@gmail.com>
2026-08-23 22:18:09 +0200

    exp: drop -v5, keep tcpdump (isolate which instrument changes the outcome)

--- tests/17-tun-rpl-br/test-native-border-router.sh
+++ tests/17-tun-rpl-br/test-native-border-router.sh
@@ -23,7 +23,7 @@ COUNT=5
 
 # Connect to the simulation
 echo "Starting native border-router"
-make -C $CONTIKI/examples/rpl-border-router -B connect-router-cooja TARGET=native ROUTER_ARGS="-v5 fd00::1/64" &
+make -C $CONTIKI/examples/rpl-border-router -B connect-router-cooja TARGET=native &
 MPID=$!
 printf "Waiting for network formation (%d seconds)\n" "$WAIT_TIME"
 sleep $WAIT_TIME
